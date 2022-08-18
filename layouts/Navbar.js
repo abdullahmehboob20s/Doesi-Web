56ef821b5444188a54e67a86ff45c303da7a6149
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-08-18 21:54:47 +0500

    Mobile Design Done For Navbar

--- layouts/Navbar.js
+++ layouts/Navbar.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { IoMenu } from "react-icons/io5";
 
 function Navbar() {
   return (
-    <div>
-      <div className="container">
-        <h1>Navbar</h1>
+    <div className="fixed top-0 w-full">
+      <div className="container py-[1.6rem] flex items-center space-x-[1rem]">
+        <img src="images/logo.svg" className="cursor-pointer" alt="" />
+
+        <div className="flex items-center justify-between flex-1">
+          <button className="button text-base font-bold">Dashboard</button>
+
+          <button className="text-[1.8rem] flex">
+            <IoMenu />
+          </button>
+        </div>
       </div>
     </div>
   );
