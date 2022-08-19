5be887ae55473e86bf15dfccc5bedf2c9b4e841a
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-08-19 16:08:02 +0500

    Desktop Design Done For Footer

--- layouts/Footer.js
+++ layouts/Footer.js
@@ -4,19 +4,19 @@ import { FaTwitter, FaDiscord } from "react-icons/fa";
 function Footer() {
   return (
     <div className="bg-[#1E2644]">
-      <div className="container flex items-center justify-between py-[1.2rem] space-x-4">
-        <img src="images/logo.svg" className="w-[5rem]" alt="" />
+      <div className="container flex items-center justify-between py-[1.2rem] space-x-4 xl:py-6">
+        <img src="images/logo.svg" className="w-[5rem] md:w-[8rem]" alt="" />
 
-        <p className="text-center text-white font-bold text-[.8rem]">
+        <p className="text-center text-white font-bold text-[.8rem] md:text-lg">
           Dosei. All Rights Reserved.
         </p>
 
-        <div className="flex items-center space-x-3">
-          <a href="#" className="text-white text-xl">
-            <FaTwitter />
-          </a>
-          <a href="#" className="text-white text-xl">
+        <div className="flex items-center space-x-3 md:space-x-6">
+          <a href="#" className="text-white text-xl md:text-[1.8rem]">
             <FaDiscord />
+          </a>
+          <a href="#" className="text-white text-xl md:text-[1.6rem]">
+            <FaTwitter />
           </a>
         </div>
       </div>
